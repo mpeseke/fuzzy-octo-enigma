c777b3119d278648f9090449e4cc7d65f0633798
Marlon Oliver Peseke <mpeseke@gmail.com>
2022-03-03 16:59:01 -0700

    added some style

--- frontend/src/views/CreateUser.vue
+++ frontend/src/views/CreateUser.vue
@@ -1,9 +1,26 @@
 <template>
+    <div class="container">
+        <main class="form-signin">
+            <form>
+                <h1 class="h3 m-3 fw-normal">Please sign in</h1>
 
-    <div class="createUser">
-        <h1>{{count}}</h1>
-        <input type="number" v-model="count" />
-        
+                <div class="form-floating">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+                </div>
+
+                <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
+                </div>
+                <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+            </form>
+        </main>
     </div>
 </template>
 <script>
@@ -11,15 +28,35 @@ import { ref } from 'vue'
 
 export default {
     name: "CreateUser",
-    setup() {
-        const count = ref(0);
 
-        return {
-            count
-        }
-    }
 }
 </script>
-<style>
+<style scoped>
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+
+.form-signin .checkbox {
+  font-weight: 400;
+}
+
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
     
 </style>
